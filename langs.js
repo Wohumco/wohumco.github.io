5da4cad31624d6477996bc5871494d34410f1426
Wohumco <wohumco@gmail.com>
2025-10-16 17:21:16 +0200

    Update langs.js

--- langs.js
+++ langs.js
@@ -5,119 +5,92 @@ const translations = {
     Title: `<div style="font-family: offside;">Worldwide Humanic Coalition</div>`,
     GoalsAndSolutions: `1✧︎ Goals and Solutions`,
     GoalsAndSolutionsText: `
-      Постройте своими руками мир, в котором хочется жить вечно! <br /> <br />
-  Мы стремимся к неизменной долгосрочной ЦЕЛИ - прийти к <br />
-  принципиально новому и стабильному варианту устройства цивилизации, <br />
-  где все разумные существа без исключения живут в благополучии, мире и процветании,<br />
-  счастливо и беззаботно, имея свободу и возможность развиваться во всех аспектах жизни, <br />
-   свободно расширять горизонты познания мироустройства и всем вместе <br />
-  достичь ранее невиданного расцвета цивилизации и гармонии в мире.<br /><br />
-  
-Мы стремимся защитить людей, улучшать наши жизни и закрепить людские свободы, <br />
-   идущие от нашей природы, как истинно гуманного, исконно непорочного и искреннего <br />
-   человеческого существа, а также совместными усилиями решать наши проблемы и трудности.<br /><br />
-  
-Руководствуясь принципами <br />
-  Гуманизма и Благодетельности, Милосердия, Заботы и Взаимопомощи, <br />
-  Справедливости и Рассудительности, Равноправия, Сплочённости и Взаимоуважения, <br />
-  Прогрессивности, Открытости и Прозрачности,<br />
-С бесконечной любовью и верой в людей, надеждой на счастливое будущее и c наилучшими пожеланиями,<br />
-Первый Когитор<br /><br />
+      Build with your own hands a world you would want to live in forever!
 
-ОСНОВНЫЕ РЕШЕНИЯ:<br />
-Отсутствие денежной системы и ей подобных. Все ресурсы являются РАВНОРАСПРЕДЕЛЁННЫМ ОБЩЕЧЕЛОВЕЧЕСКИМ БОГАТСТВОМ.<br />
-(Больше никто и ничего не отнимет у людей основываясь на безраздельном захватническом владении.<br />
- Не существует абстрактных "капитала" или "экономики", которым будет лучше, если все будут жить хуже. <br />
- Всегда были лишь конкретные (люди) выгодополучатели. Необходимо, чтобы прогресс, оптимизация и изменения <br />
- приносили равномерное улучшение качества жизни каждому человеку и всему обществу целиком.)<br /><br /> 
+We strive toward a constant, long-term GOAL — to reach a fundamentally new and stable model of civilization, where all sentient beings without exception live in well-being, peace, and prosperity, happily and carefree, enjoying freedom and opportunities for growth in every aspect of life, freely expanding the horizons of understanding reality, and all together achieving an unprecedented flourishing of civilization and harmony in the world.
 
-Все люди могут использовать блага свободно. Каждый обладает неотъемлемым правом на ПОЛНОЕ и БЕЗВОЗМЕЗДНОЕ<br />
-удовлетворение всех потребностей для качественной жизни. <br />
-(Так пропадают мотивы для свершения подавляющего большинства преступлений всех масштабов)<br /><br />
+We aim to protect people, improve our lives, and secure the fundamental human freedoms, rooted in our very nature — as truly humane, inherently pure, and sincerely caring human beings, and solve our shared problems and overcome difficulties together.
 
-ОБШИРНЫЕ ПРАВА ЧЕЛОВЕКА, которые универсально признаются и защищаются.<br />
-ИСКОРЕНЕНИЕ ПРИТИСНЕНИЙ и РАЗДЕЛЕНИЯ по любым признакам и любой ЭКСПЛУАТАЦИИ людей как явления.<br />
-Универсальная ЗАКОНОДАТЕЛЬНАЯ СПРАВЕДЛИВОСТЬ и ПОЛНАЯ ПРОЗРАЧНОСТЬ действий и процедур. <br />
-ОБЩЕДОСТУПНОСТЬ любой (не личной) ИНФОРМАЦИИ для ознакомления и использования в любой сфере, <br />
-с сохранением авторства. СВОБОДА СЛОВА и ВЫРАЖЕНИЯ.<br />
-Максимальное ИЗБАВЛЕНИЕ ОТ БЮРОКРАТИИ, карательного бумажкособирательства и формализма.<br />
-Отсутствие давления на личность, возможность создания своего личного счастья/мечты/дела.<br />
-Искоренение негуманных идеологий, решений и методов.<br />
-Отсутствие избыточных трат ресурсов на сомнительные и бесполезные отрасли.<br />
-Фильтрация, оптимизация и избавление от мусорного, бесполезного и вредоносного контента.<br /><br />
+Guided by the principles of:
+ Humanism and Benevolence, Compassion, Care and Mutual Support,
+ Justice and Prudence, Equality, Solidarity and Mutual Respect,
+ Progressiveness, Openness, and Transparency.
+With endless love and belief in humanity, hope for a happy future, and our best wishes,
+ The First Cogitor
 
-Мощные системы социальной поддержки сопровождают людей с детства и <br />
-на всех этапах жизни: социализация, знакомства, учёба, работа, <br />
-решение конфликтов, создание и поддержание комфортной среды для жизни.<br /><br />
+CORE SOLUTIONS:
+Abolition of the monetary system and all its equivalents.
+ All resources are to be considered the EQUALLY SHARED COMMON WEALTH of humanity. 
+(No one will ever again be deprived of anything based on someone's absolute ownership or conquest. There do not exist any abstract entities like “the capital” or “the economy” that "benefit" from widespread human suffering. There have only ever been real, individual beneficiaries. Progress, optimization, and change must now lead to equal quality of life improvements for every individual and society as a whole.)
 
-Переработанное общедоступное и полностью бесплатное на всех этапах образование, <br />
-нацеленное воспитывать и обучать людей более гуманными и сознательными, <br />
-оставлять в них стремление развиваться и поддерживать мир и гармонию, внутри и вокруг. <br />
-Концентрация на реальных остающихся знаниях и практических навыках, <br />
-упразднение оценок в учебных заведениях. (Человек может учиться дольше чем обычно,<br />
-без спешки, сколько понадобится, но будет более приспособлен к жизни и профессии).<br /><br />
+All the people do have free access to all essential goods.
+ Every person has the inalienable right to the FULL and UNCONDITIONAL satisfaction of all basic needs for a good quality life.
+ (This eliminates the vast majority of motivations for crimes at all levels.)
 
-Гибкая и индивидуализированная бесплатная система здравоохранения и медицинской помощи. <br />
-Гуманные программы терапии, помощи и реабилитации для девиантных людей, <br />
-вместо бездумного клеймления изгоями социума. <br /><br />
+EXTENSIVE HUMAN RIGHTS which are universally recognized and protected.
+ERADICATION OF OPPRESSION and DIVISION of any kind, and ABOLITION OF EXPLOITATION of humans in all forms.
+Universal LEGISLATIVE JUSTICE and FULL TRANSPARENCY of all actions and procedures.
+FULL PUBLIC ACCESSIBILITY of all (non-personal) INFORMATION for awareness and use across all fields — with authorship preserved.
+FREEDOM OF SPEECH and EXPRESSION.
+Maximum ELIMINATION OF BUREAUCRACY, punitive paperwork collecting, and formalism.
+No pressure on individual personality — the freedom to build your own happiness, dreams, and purpose.
+Abolition of inhumane ideologies, solutions, and methods.
+No wasteful spending of resources on questionable or pointless sectors.
+Filtering, optimization, and removal of harmful, trash, or useless web content.
 
-Забота об окружающей среде и поддержка её благосостояния. <br />
-Строгий контроль за экологией, поддержание состояния территорий, <br />
-очистка и минимизация следа от жизнедеятельности.<br />
-Продвинутая агрономия и более этичные и безопасные источники пищи. <br />
-Полный переход на нетоксичные производства и безопасные экологичные альтернативы <br />
-(Больше не будет случаев когда мегакорпорации отравляют буквально всю биоферу). <br />
-Использование исключительно чистых источников и способов извлечения энергии <br />
-(которые реально не вредят природе, а не те что лишь продвигаются как безопасные). <br />
-Продвинутые решения в области использования условно неиссякаемых источников энергии:<br />
-солнце, ветер, течения и приливы, геологическое тепло и холодный синтез.<br /><br />
+Strong social support systems accompanying people from childhood through all life stages: socialization, relationships, education, work, conflict resolution,
+ and the creation of a comfortable living environment.
 
-Максимальная автоматизация всех сфер (в связи с отсутствием нужды полной занятости<br />
-и искусственного создания рабочих мест). <br />
-Внедряется строгий отказ от запланированного устаревания<br />
-и преднамеренных поломок при производстве продукции (этот вредительский капиталистический атавизм<br />
-теперь бесполезен). Продукт производится максимально качественным и долговечным, <br />
-независимым от поддержки, неприхотливым к обслуживанию и условиям, и дружелюбным к пользователю. <br />
-Использование полезных и практичных технический решений, отклонённых из-за <br />
-невыгодности капиталистической машине и излишней прогрессивности
-(слишком шороший продукт, замену/улучшение которого не произведёшь и не продашь).<br /><br />
+Reformed, universally accessible, and completely free education at all levels,
+ aimed at raising more humane and conscious individuals, fostering a desire to grow and maintain peace and harmony, outside and within.
+ Focus is placed on real, lasting knowledge and practical skills, abolishment of grading systems. (People can learn at their own pace, however long it takes, graduating more prepared for life and profession.)
 
-Институты, их Исследования и Разработки не будут нуждаться в постоянном выбивании финансирования, <br />
-специфичных разрешений и специалистов, что сильно ускорит и упростит научный прогресс.<br /> 
-Мы выступаем ЗА ПРОГРЕСС И ВСЕСТОРОННЕЕ УЛУЧШЕНИЕ КАЧЕСТВА ЖИЗНИ, <br />
-не руководствуясь бездумной и бесчеловечной максимизацией выгоды.<br /><br />
+Strong, Flexible and Individualized, completely free healthcare and medical support.
+ Humane therapy, support, and rehabilitation programs for deviant individuals, instead of stigmatization and social exile.
 
-Корпорации могут открывать филиалы и производить технологичную продукцию на <br />
-особых условиях новой экономической зоны.<br /><br />
+Environmental care and ecosystem welfare.
+ Strict ecological control, territory upkeep, waste reduction and ecological footprint minimization.
+ Advanced agronomy and more ethical and safe food sources.
+ Complete shift to non-toxic production and safe, eco-friendly alternatives
+ (No more cases where megacorporations poison entire ecosystems).
+ Use of exclusively clean, truly non-harmful energy sources and methods of energy extraction (which actually do no harm to nature, not just those marketed as “green”). Advanced solutions in field of of truly sustainable energy sources:
+ solar, wind, tides and currents, geothermal heat, and cold fusion.
 
-Рабочие должности квотируются и распределяются в соответствии с предпочтениями и запросами людей, <br />
-но люди не вынуждены работать чтобы выжить. Вечно отдыхать невозможно, <br />
-а любимое дело наоборот приносит человеку счастье и чувство удовлетворения от самореализации. <br />
-Труд распределяется по меритократическому принципу, где задачи даются по способностям, <br />
-и те, кто также способен помочь в достижении решения, объединяют усилия воедино. <br />
-Незанятые люди могут отдыхать, быть заняты ремеслом, искусством, домохозяйством, волонтёрством, <br />
-хобби, саморазвитием, путешествиями, рекреацией и др. <br />
-или добровольно перераспределены на выбраные должности. <br />
+Maximum automation of all sectors (since imitative full employment is no longer necessary when people aren't forced to have a job to survive.)
+ No more planned obsolescence or intentional product breakdowns (this parasitic capitalist relic is obsolete). Products will be made to be as durable, high-quality, independent and user-friendly as possible,
+ Use of previously discarded clever and practical technologies and solutions, rejected due to their unprofitability for the capitalist machine and excessive progressiveness (a product that is too good and cannot be replaced/improved and sold again).
 
+Universities, their Researches and Developments will no longer need to fight for funding, permissions, or experts, dramatically accelerating scientific progress.
+ We support PROGRESS AND HOLISTIC IMPROVEMENT OF LIFE QUALITY,
+ not being guided by blind and inhumane profit maximization.
 
-<br /><br />
-(Наши основные направления научных интересов на текущий момент:<br />
-Радикальное продление жизни и улучшение её качества, развитие медицины и жизненного пространства.<br />
-Изучение фундаментального устройства реальности.<br />
-Изучение, развитие и улучшение возможностей человеческого сознания и тела.<br />
-Развитие методов очистки, сохранения и восстановления окружающей среды.<br />
-Развитие методов защиты цивилизации от масштабных угроз, <br />
-диверсификация областей обитания человечества.<br />
-Исследование продвинутых и неконвенциональных источников энергии и развитие существующих. <br />
-Исследование продвинутых и неконвенциональных методов транспортировки<br />
-и перемещения, а также улучшение существующих.<br />
-Изучение нестандартных моделей социальных структур,<br />
-и др.)<br /><br />
+Corporations can open branches and produce tech and products under the special conditions of the new economic zone. 
 
-Присоединяйтесь чтобы узнать больше! <br />
-Мы будем рады поделиться идеями, подробностями и видением <br />
-А если Вам есть что добавить, мы открыты для предложений и готовы к доработке решений.<br />
-Давайте вместе начинать строить лучшее будущее уже сейчас!<br /><br /></p> `
+Jobs are offered according to personal preferences and needs,
+but people are not forced to work to survive. It's impossible to rest forever,
+but a favorite activity, on the contrary, brings joy and a sense of fulfillment.
+Labor is distributed according to meritocratic principles, where tasks match abilities, and those capable of helping unite their efforts to achieve common goals.
+Unemployed people may rest, be occupied by crafts, art , housework, volunteering,
+hobbies, self-development, travel, recreation, etc.
+or voluntarily redeployed to chosen positions.
+
+Our current scientific areas of interest are: 
+Radical  lifetime extension and and quality of life improvement, medical advancements and expanding livable space.
+Study of the fundamental nature of reality.
+Study, expansion, and improvement of human mind and body capabilities .
+Development of methods for depolluting, preserving, and restoring the environment.
+Development of methods for protecting civilization from large-scale threats, human habitats diversification.
+Research of advanced and unconventional energy sources, as well as the improvement  of existing ones.
+Research of advanced and unconventional methods of transportation
+and movement, of existing ones.
+Study of alternative social structure  models,
+and more.
+
+Join us to learn more!
+We are happy to share ideas, details, and vision.
+If you have something to contribute, feel free to contact. 
+We're open to suggestions, discussion and ready to improve solutions.
+Let's start building a better future together right now!`
   },
   ru: {
     HeadTitle: `Всемирная гуманистическая коалиция`,
@@ -236,6 +209,6 @@ const translations = {
 Присоединяйтесь чтобы узнать больше! <br />
 Мы будем рады поделиться идеями, подробностями и видением <br />
 А если Вам есть что добавить, мы открыты для предложений и готовы к доработке решений.<br />
-Давайте вместе начинать строить лучшее будущее уже сейчас!<br /><br /></p> `
+Давайте вместе начинать строить лучшее будущее уже сейчас!<br /><br /> `
   }
 };
